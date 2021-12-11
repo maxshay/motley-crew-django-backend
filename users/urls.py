12897dbc85fddb2c46640ea13034c18a00ee91ab
Maksim Shaynyuk <maksimshaynyuk@gmail.com>
2021-12-11 10:30:00 -0800

    login, logout api route, added protected route

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+from .views import GetCSRFToken, LogInView, LogOutView, CheckAuthenticatedView, UserInfoView
 
 
 urlpatterns = [
+  path('csrf_cookie', GetCSRFToken.as_view()),
+  path('auth/login', LogInView.as_view()),
+  path('auth/logout', LogOutView.as_view()),
+  path('auth/loggedIn', CheckAuthenticatedView.as_view()),
+  path('profile', UserInfoView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,82 @@
 from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.response import Response
+from .models import User
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
+from django.utils.decorators import method_decorator
+from django.contrib import auth
+
+
+from .serializers import UserSerializer
 
 # https://www.youtube.com/watch?v=NFHiT4ncPD8
+
+
+
+
+@method_decorator(csrf_protect, name='dispatch')
+class CheckAuthenticatedView(APIView):
+  def get(self, request, format=None):
+    isAuthenticated = User.is_authenticated
+
+    if isAuthenticated:
+      return Response({'error': False, 'message': {'authenticated': True}})
+    else:
+      return Response({'error': False, 'message': {'authenticated': False}})
+
+
+@method_decorator(csrf_protect, name='dispatch')
+class LogInView(APIView):
+  permission_classes = (permissions.AllowAny,)
+  def post(self, request, format=None):
+    data = self.request.data
+
+    missing = []
+    if 'username' not in data:
+      missing.append('username')
+    if 'password' not in data:
+      missing.append('password')
+        
+    if len(missing) > 0:
+      return Response({'error': True, 'message': ", ".join(missing) + " missing in json body"})
+    
+    username = data['username']
+    password = data['password']
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is None:
+      return Response({'error': True, 'message': 'username or password is incorrect'})
+    else:
+      auth.login(request, user)
+      return Response({'error': False, 'message': 'login success', 'data': {'username': username}})
+
+
+class LogOutView(APIView):
+  def post(self, request, format=None):
+    try:
+      auth.logout(request)
+      return Response({'error': False, 'message': 'user logged out'})
+    except:
+      return Response({'error': True, 'message': 'user could not be logged out'})
+
+
+class UserInfoView(APIView):
+  def get(self, request, format=None):
+    user = self.request.user
+
+    print(user)
+
+    user = User.objects.get(id=user.id)
+
+    user_info = UserSerializer(user)
+
+    return Response({'error': False, 'data': user_info.data})
+
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class GetCSRFToken(APIView):
+  permission_classes = (permissions.AllowAny,)
+
+  def get(self, request, format=None):
+    return Response({'error': False, 'message': 'CSRF cookie set'})

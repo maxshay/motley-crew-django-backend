063a5b12989e1388df1358793327b104be55fe96
Maksim Shaynyuk <maksimshaynyuk@gmail.com>
2022-02-11 15:39:52 -0800

    add cors suport all

--- motleycrew_backend/settings.py
+++ motleycrew_backend/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
   'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost', 'http://127.0.0.1', 'https://motleycrewdev.com']
+CORS_ALLOWED_ORIGINS = ['http://localhost', 'http://127.0.0.1', 'https://motleycrewdev.com', '*']
 
 ROOT_URLCONF = 'motleycrew_backend.urls'
 

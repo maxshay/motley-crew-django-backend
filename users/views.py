98083d391dcdc76e395a101dab75eb09a0e47587
Maksim Shaynyuk <maksimshaynyuk@gmail.com>
2022-02-13 15:40:47 -0800

    more api doc details

--- users/views.py
+++ users/views.py
@@ -13,9 +13,9 @@ class CheckAuthenticatedView(APIView):
     isAuthenticated = User.is_authenticated
 
     if isAuthenticated:
-      return Response({'error': False, 'message': {'authenticated': True}})
+      return Response({'error': False, 'message': {'authenticated': True}, 'data': None})
     else:
-      return Response({'error': False, 'message': {'authenticated': False}})
+      return Response({'error': False, 'message': {'authenticated': False}, 'data': None})
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -31,14 +31,14 @@ class LogInView(APIView):
       missing.append('password')
         
     if len(missing) > 0:
-      return Response({'error': True, 'message': ", ".join(missing) + " missing in json body"})
+      return Response({'error': True, 'message': ", ".join(missing) + " missing in json body", 'data': None})
     
     username = data['username']
     password = data['password']
     user = auth.authenticate(username=username, password=password)
 
     if user is None:
-      return Response({'error': True, 'message': 'username or password is incorrect'}, status=400)
+      return Response({'error': True, 'message': 'username or password is incorrect', 'data': None}, status=400)
     else:
       auth.login(request, user)
       return Response({'error': False, 'message': 'login success', 'data': {'username': username}})
@@ -49,9 +49,9 @@ class LogOutView(APIView):
   def post(self, request, format=None):
     try:
       auth.logout(request)
-      return Response({'error': False, 'message': 'user logged out'})
+      return Response({'error': False, 'message': 'user logged out', 'data': None})
     except:
-      return Response({'error': True, 'message': 'user could not be logged out'})
+      return Response({'error': True, 'message': 'user could not be logged out', 'data': None})
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -62,7 +62,7 @@ class UserInfoView(APIView):
     user = User.objects.get(id=user.id)
     user_info = UserSerializer(user)
 
-    return Response({'error': False, 'data': user_info.data})
+    return Response({'error': False, 'message: 'ok', 'data': user_info.data})
 
 
 @method_decorator(ensure_csrf_cookie, name='dispatch')
@@ -70,4 +70,4 @@ class GetCSRFToken(APIView):
   permission_classes = (permissions.AllowAny,)
 
   def get(self, request, format=None):
-    return Response({'error': False, 'message': 'CSRF cookie set'})
+    return Response({'error': False, 'message': 'CSRF cookie set', 'data': None})

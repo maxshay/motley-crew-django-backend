3d82465c049193f2494f4282d4b6ae8be470a0f0
Ferris-Bueller7 <46939195+Ferris-Bueller7@users.noreply.github.com>
2022-02-23 20:17:37 -0800

    backend testing started

--- folders/tests.py
+++ folders/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+import base64 # new
+import json # new
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+
+
+#fix data base error permission denided to create database
+class TestTest(APITestCase):
+    def test_test(self):
+        self.assertEqual(1,1)

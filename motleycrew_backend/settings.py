ec292a0ff2e1f7d43800b078d85f70d36b47a1d4
Maksim Shaynyuk <35791051+maxshay@users.noreply.github.com>
2022-02-28 22:15:03 -0800

    change jwt exp

--- motleycrew_backend/settings.py
+++ motleycrew_backend/settings.py
@@ -161,8 +161,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-  'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=30),
-  'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+  'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1),
+  'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=10),
   'USER_ID_CLAIM': 'id',
 }
 

2fb96abc7dbe7d58a6490f944fbb09a68bdd39fe
Maksim Shaynyuk <35791051+maxshay@users.noreply.github.com>
2022-02-28 18:19:41 -0800

    allow all correct spelling

--- motleycrew_backend/settings.py
+++ motleycrew_backend/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
 ]
 
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://motleycrewdev.com']
 # CORS_ORIGIN_WHITELIST = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://motleycrewdev.com']
 

e50728fc7a396955c3ffde3fab6bf97fd13381ca
Maksim Shaynyuk <35791051+maxshay@users.noreply.github.com>
2022-02-28 19:55:29 -0800

    update reqs file

--- folders/tests.py
+++ folders/tests.py
@@ -1,15 +1,11 @@
 from django.test import TestCase
-import base64 # new
-import json # new
+import base64
+import json
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
-
-
-
-#fix data base error permission denided to create database
 class TestTest(APITestCase):
     def test_test(self):
         self.assertEqual(1,1)

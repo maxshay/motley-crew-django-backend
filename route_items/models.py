16e8cb85a990876edb0d1b0e57fd414739c99caa
Maksim Shaynyuk <35791051+maxshay@users.noreply.github.com>
2022-03-14 20:21:13 -0700

    MC-54 create route item app

---
+++ route_items/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class RouteItem(models.Model):
+  # route_slip_id - route slip ref
+  # person (assignee) - user model ref
+  # type - string (view, sign)
+  # order_num - integer
+  # complete - boolean
+  # comments - string
+  pass
